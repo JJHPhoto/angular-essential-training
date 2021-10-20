7777f86c869f2bb2d2f8cf0eaeaf61f812c9083c
Joshua J. Haller <phone@joshuajhaller.com>
2021-10-20 13:33:21 -0700

    Added FormsModule to AppModule. Added form files from cloned repo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { MediaItemComponent } from "./media-item.component";
 import { MediaItemListComponent } from "./media-item-list.component";
@@ -7,7 +8,7 @@ import { FavoriteDirective } from "./favorite.directive";
 import { CategoryListPipe } from "./category-list.pipe";
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
     MediaItemComponent,
